a2259a1a7c8fd29595f9a46efa299611ea629146
HeyIAmEj <everton7ej@gmail.com>
2023-12-16 17:01:41 -0300

    timestamp and time changing

--- FirebaseRTDB/FirebaseRTDB.ino
+++ FirebaseRTDB/FirebaseRTDB.ino
@@ -10,7 +10,6 @@
  *
  */
 
-
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <addons/TokenHelper.h>
@@ -25,7 +24,7 @@
 #define API_KEY "AIzaSyByZl9ncf8UoIk6WEPkzoq_peLubHAatNY"
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "https://vumi-9db3e-default-rtdb.firebaseio.com"  //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_URL "https://vumi-9db3e-default-rtdb.firebaseio.com" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "vumiesp@vumiesp.com"
@@ -39,19 +38,20 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
-unsigned long interval = 15000;
+unsigned long interval = 20 * 1000;
 unsigned long count = 0;
 
-void setup() {
+void setup()
+{
 
   Serial.begin(115200);
-
 
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   Serial.print("Connecting to Wi-Fi");
   unsigned long ms = millis();
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(300);
   }
@@ -71,7 +71,6 @@ void setup() {
 
   config.token_status_callback = tokenStatusCallback;
 
-
   Firebase.reconnectNetwork(true);
 
   fbdo.setBSSLBufferSize(4096 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
@@ -81,42 +80,43 @@ void setup() {
 
   Firebase.begin(&config, &auth);
 
-
-
   Firebase.setDoubleDigits(5);
 
   config.timeout.serverResponse = 10 * 1000;
-  //Network reconnect timeout (interval) in ms (10 sec - 5 min) when network or WiFi disconnected.
+  // Network reconnect timeout (interval) in ms (10 sec - 5 min) when network or WiFi disconnected.
   config.timeout.networkReconnect = 10 * 1000;
-  //will return false (error) when it called repeatedly in loop.
+  // will return false (error) when it called repeatedly in loop.
   config.timeout.rtdbStreamError = 3 * 1000;
 }
 
-void tokenStatusCallback() {
+void tokenStatusCallback()
+{
   Serial.println("Data");
 }
 
-void loop() {
+void loop()
+{
 
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > interval || sendDataPrevMillis == 0)) {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > interval || sendDataPrevMillis == 0))
+  {
     sendDataPrevMillis = millis();
 
     FirebaseJson mainJson;
 
     createJson(mainJson, "name", "LG Dual Voice");
     createJson(mainJson, "status", "on");
-
-
+    // createJson(mainJson, "timestamp", Firebase.RTDB.setTimestamp());
+    mainJson.set("timestamp/.sv", "timestamp");
+    
     bool result = Firebase.RTDB.set(&fbdo, DEVICE_NAME, &mainJson);
     Serial.printf("Enviando payload.... \n %s \n\n", result ? "Sucesso!" : fbdo.errorReason().c_str());
-
 
     count++;
   }
 }
 
-
-FirebaseJson createJson(FirebaseJson& data, const String& key, const String& value) {
-    data.add(key, value);
-    return data;
+FirebaseJson createJson(FirebaseJson &data, const String &key, const String &value)
+{
+  data.add(key, value);
+  return data;
 }
